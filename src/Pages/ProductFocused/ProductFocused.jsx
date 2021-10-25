f70c672617aa9e0aab5a5e4e65a9b18053cda348
Himitsu Fushigi <himitsu.fushigi@protonmail.com>
2021-10-25 11:49:11 +0545

    Search bar and product details title added

--- src/Pages/ProductFocused/ProductFocused.jsx
+++ src/Pages/ProductFocused/ProductFocused.jsx
@@ -1,16 +1,50 @@
-import React from 'react'
-import './style.scss'
+import React from "react";
+import "./style.scss";
 
 // @components
 import Sidebar from "../../Components/Sidebar/Sidebar";
-import ProductCard from "../../Components/ProductCard/ProductCard";
 import CheckoutButton from "../../Components/CheckoutButton/CheckoutButton";
 
+// @packages
+import { Breadcrumbs, Button, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
 export default function ProductFocused() {
-    return (
-        <div className="productFocused">
-            <CheckoutButton/>
-            <Sidebar/>
+  return (
+    <div className="productFocused">
+      <CheckoutButton />
+      <Sidebar />
+
+      <div className="productFocused__main">
+        {/* @section search bar */}
+        <div className="productFocused__main__searchBar">
+          <input type="text" placeholder="Search Marketplace" />
+          <Button>
+            <i className="ri-search-line"></i>
+          </Button>
         </div>
-    )
+
+        {/* @section product details */}
+        <div className="productFocused__main__productDetails">
+          <h2 className="productFocused__main__productDetails__title">
+            Product Details
+          </h2>
+          <Breadcrumbs aria-label="breadcrumb">
+            <Link
+              underline="hover"
+              color="inherit"
+              href="/getting-started/installation/"
+            >
+              Ecommerce
+            </Link>
+            <Typography color="text.primary">
+              Nike air force 1 ndestrukt
+            </Typography>
+          </Breadcrumbs>
+        </div>
+
+        {/* @section product card */}
+      </div>
+    </div>
+  );
 }
