fc17007671c55408c60a2dc5cea5cb73de1b2110
Himitsu Fushigi <himitsu.fushigi@protonmail.com>
2021-10-27 10:31:30 +0545

    Product Banner, Card Image, Description and Review also added

--- src/Pages/ProductFocused/ProductFocused.jsx
+++ src/Pages/ProductFocused/ProductFocused.jsx
@@ -8,8 +8,50 @@ import CheckoutButton from "../../Components/CheckoutButton/CheckoutButton";
 // @packages
 import { Breadcrumbs, Button, Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
 
 export default function ProductFocused() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <div className="productFocused">
       <CheckoutButton />
@@ -45,10 +87,71 @@ export default function ProductFocused() {
 
         {/* @section product display */}
         <div className="productFocused__main__productDisplay">
-            this is display
+          <img
+            src="https://minimals.cc/static/mock-images/products/product_1.jpg"
+            alt=""
+          />
+          <div className="productFocused__main__productDetails__content"></div>
         </div>
 
-        
+        {/* @section banner */}
+        <div className="productFocused__main__banner">
+          <div className="productFocused__main__banner__item">
+            <div className="productFocused__main__banner__item__icon">
+              <i className="ri-star-fill"></i>
+            </div>
+            <div className="productFocused__main__banner__item__title">
+              100% Original
+            </div>
+            <div className="productFocused__main__banner__item__content">
+              Lorem ipsum dolor sit amet consequer los noramles
+            </div>
+          </div>
+          <div className="productFocused__main__banner__item">
+            <div className="productFocused__main__banner__item__icon">
+              <i className="ri-time-fill"></i>
+            </div>
+            <div className="productFocused__main__banner__item__title">
+              10 Day Replacement
+            </div>
+            <div className="productFocused__main__banner__item__content">
+              Lorem ipsum dolor sit amet consequer los noramles
+            </div>
+          </div>
+          <div className="productFocused__main__banner__item">
+            <div className="productFocused__main__banner__item__icon">
+              <i className="ri-shield-cross-fill"></i>
+            </div>
+            <div className="productFocused__main__banner__item__title">
+              Year Warranty
+            </div>
+            <div className="productFocused__main__banner__item__content">
+              Lorem ipsum dolor sit amet consequer los noramles
+            </div>
+          </div>
+        </div>
+
+        {/* @section description and reviews */}
+        <div className="productFocused__main__descriptionAndReviews">
+          <Box sx={{ width: "100%" }}>
+            <Box className="productFocused__main__descriptionAndReviews__titleBar" sx={{ borderBottom: 1, borderColor: "divider" }}>
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                aria-label="basic tabs example"
+              >
+                <Tab style={{textTransform: "initial"}} label="Description" {...a11yProps(0)} />
+                <Tab style={{textTransform: "initial"}} label="Reviews" {...a11yProps(1)} />
+              </Tabs>
+            </Box>
+            <TabPanel className="productFocused__main__descriptionAndReviews__content" value={value} index={0}>
+              Description
+            </TabPanel>
+            <TabPanel className="productFocused__main__descriptionAndReviews__content" value={value} index={1}>
+              Reviews
+            </TabPanel>
+          </Box>
+        </div>
       </div>
     </div>
   );
